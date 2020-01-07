f187224b96771fbc589d2914e7c8674b8c3b6b67
Jake Carpenter <jake@jakecarpenter.dev>
2020-01-07 14:54:33 -0700

    Build basic V1 endpoints

---
+++ ApiVersioningDemo/Todos/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiVersioningDemo.Todos
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ ApiVersioningDemo/Todos/TodoControllerV1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiVersioningDemo.Todos
+{
+    [ApiController]
+    [Route("todos")]
+    public class TodoControllerV1 : ControllerBase
+    {
+        public static List<Todo> Todos = new List<Todo>
+        {
+            new Todo { Id = 1, Value = "one" },
+            new Todo { Id = 2, Value = "two" },
+            new Todo { Id = 3, Value = "three" },
+            new Todo { Id = 4, Value = "four" },
+            new Todo { Id = 5, Value = "five" },
+        };
+
+        [HttpGet]
+        public ActionResult<List<Todo>> List()
+        {
+            return Todos;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Todo> Get(int id)
+        {
+            return Todos.Find(x => x.Id == id);
+        }
+    }
+}
